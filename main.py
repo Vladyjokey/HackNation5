d69f89134845910a16a662313b53c27267d4bf92
Vladyjokey <92403896+Vladyjokey@users.noreply.github.com>
2026-04-26 02:56:38 +0300

    removed junk code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.cors import CORSMiddleware
 import os
 from supabase import create_client, Client
 
